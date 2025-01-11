07238c7667f74984c7354d01e1ebd74b21cd8d2a
Volminoduro <steve.massicard@gmail.com>
2025-01-11 17:37:05 +0100

    Testing for rune filtering

--- swex-filter.Tests/Controllers/FilterControllerTests.cs
+++ swex-filter.Tests/Controllers/FilterControllerTests.cs
@@ -1,10 +1,44 @@
-﻿namespace SwexFilter.Tests.Controllers;
+﻿using SwexFilter.Controllers;
+using SwexFilter.Data;
+using SwexFilter.Models;
+
+namespace SwexFilter.Tests.Controllers;
 
 public class FilterControllerTests
 {
-    [Fact]
-    public void ApplyFiltersToRune()
+    [Theory]
+    [InlineData(null, null, null, null, null, null, null, null, true)]
+    public void ApplyFiltersToRune(bool isActive,
+        string? name,
+        int? RelativeScore,
+        int? SubPropertiesPresence,
+        bool ExcludeEnchantedRune,
+        bool ExcludeGrindFromScore,
+        bool KeepOnlyIfGemAvailable,
+        bool KeepOnlyIfGrindAvailable,
+        bool isTrue)
     {
-        Assert.Fail("todo");
+        // Arrange
+        var dataContext = new DataContext("filters.json", "runes.json");
+        dataContext.Filters.Clear();
+        dataContext.ImportRunes(SwexImportService.ImportRunes("Resources/FilterControllersTests.json"));
+        var filterController = new FilterController(dataContext);
+
+        dataContext.Filters.Add(new Filter
+        {
+            Name = name,
+            IsActive = isActive,
+            RelativeScore = RelativeScore,
+            SubPropertiesPresence = SubPropertiesPresence,
+            ExcludeEnchantedRune = ExcludeEnchantedRune,
+            ExcludeGrindFromScore = ExcludeGrindFromScore,
+            KeepOnlyIfGemAvailable = KeepOnlyIfGemAvailable,
+            KeepOnlyIfGrindAvailable = KeepOnlyIfGrindAvailable
+        });
+        var runeToFilter = new SwexRune();
+        // Act
+        filterController.ApplyFiltersToRune(runeToFilter);
+        // Assert
+        Assert.Equal(isTrue, runeToFilter.FiltersPassed.Any(filter1 => filter1.Name.Equals(name)));
     }
 }
